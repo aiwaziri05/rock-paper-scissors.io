9e29ed2bd770da32dbb96bcf6ee6497498698845
aiwaziri05.dev <aiwaziri05.dev@gmail.com>
2025-05-05 15:10:16 +0100

    create playRound function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
 	let computerChoice = Math.random();
 
@@ -12,4 +18,41 @@ function getComputerChoice() {
 	return computerChoice;
 }
 
-console.log(getComputerChoice());
+getComputerChoice();
+
+function getHumanChoice() {
+	let humanChoice = prompt('Enter your move e.g rock, paper, scissors', '');
+
+	return humanChoice;
+}
+
+// getHumanChoice();
+
+function playRound(humanChoice, computerChoice) {
+	if (humanChoice == 'rock' && computerChoice == 'paper') {
+		console.log(`You lose! Paper beats Rock.`);
+		computerScore += 1;
+	} else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+		console.log('You lose! Scissors beats Paper');
+		computerScore += 1;
+	} else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+		console.log('You lose! Rock beats Scissors');
+		computerScore += 1;
+	} else if (humanChoice == 'paper' && computerChoice == 'rock') {
+		console.log('You win! Paper beats Rock.');
+		humanScore += 1;
+	} else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+		console.log('You win! Scissors beats Paper.');
+		humanScore += 1;
+	} else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+		console.log('You win! Rock beats Scissors.');
+		humanScore += 1;
+	} else {
+		console.log("It's a Tie.");
+	}
+}
+
+playRound(humanSelection, computerSelection);
+
+console.log(computerScore);
+console.log(humanScore);
