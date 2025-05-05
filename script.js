11a2b8bb2ae8d60e92e94cd1a29e52beeb856f2e
aiwaziri05.dev <aiwaziri05.dev@gmail.com>
2025-05-05 14:23:53 +0100

    create getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+function getComputerChoice() {
+	let computerChoice = Math.random();
+
+	if (computerChoice <= 1 / 3) {
+		computerChoice = 'rock';
+	} else if (computerChoice <= 1 / 2) {
+		computerChoice = 'paper';
+	} else {
+		computerChoice = 'scissors';
+	}
+
+	return computerChoice;
+}
+
+console.log(getComputerChoice());
